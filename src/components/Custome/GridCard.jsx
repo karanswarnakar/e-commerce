bfd88a4e91c22cfdbc6837332801c78f191e8c4c
octaveweb <karansproject955@gmail.com>
2026-03-04 18:46:11 +0530

    Progress - Section3

---
+++ src/components/Custome/GridCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const GridCard = ({data}) => {
+  return (
+    <div className='card-content cover-image'
+      style={{
+        backgroundImage: `url("${props.img}")`,
+        backgroundPosition: `center`,
+        borderImageRepeat: `no-repeat`
+      }}
+    >
+        {/*   id: 1,
+            title: "On Fruits & Vegetables",
+            subtitle: "Unlock Up To",
+            description: "Fuel your day with the purest from nature.",
+            buttonText: "Shop Now",
+            discount: null */}
+      <div className="text-data">
+        <div className="tag">
+          Hurry! Limited Time Deals Await!
+        </div>
+
+        <h1>Nourish Your Body with Nature’s Best Vitamins!</h1>
+        <p>Sign Up Now & Save $20 on Your First Order!</p>
+
+
+        <Button text="Shop Now" />
+      </div>
+      
+      {props.span!=true? " " : <span></span>}
+    </div>
+  )
+}
+
+export default GridCard
--- src/components/Section/Section3.jsx
+++ src/components/Section/Section3.jsx
@@ -1,12 +1,55 @@
-import Cover from '../Custome/Cover'
+import GridCard from "../Custome/GridCard";
+
 const Section3 = () => {
+    const banners = [
+        {
+            id: 1,
+            title: "On Fruits & Vegetables",
+            subtitle: "Unlock Up To",
+            description: "Fuel your day with the purest from nature.",
+            buttonText: "Shop Now",
+            discount: null
+        },
+        {
+            id: 2,
+            title: "On Bakery Products",
+            subtitle: "Claim Up To",
+            description: "Fuel your day with the purest from nature.",
+            buttonText: "Shop Now",
+            discount: null
+        },
+        {
+            id: 3,
+            title: "Weekly Highlight",
+            subtitle: "Nourish Boldly, Spend Wisely!",
+            description: "Weekend Only",
+            buttonText: null,
+            discount: "30%"
+        },
+        {
+            id: 4,
+            title: "Just Arrived",
+            subtitle: "Fresh from Nature’s Farm",
+            description: "Just",
+            price: "$29.00",
+            buttonText: null
+        },
+        {
+            id: 5,
+            title: "Seasonal Special",
+            subtitle: "Tastes Like Fresh, Feels Like a Deal!",
+            description: "Enjoy an extra 25% off",
+            buttonText: null,
+            discount: "25%"
+        }
+    ];
+
+
     return (
         <div className='grid-card-content'>
-            <Cover span={false} />
-            <Cover span={false} />
-            <Cover span={false} />
-            <Cover span={false} />
-            <Cover  span={false}/>
+            {banners.map((elm,idx)=>{
+                return <GridCard span={false} data={elm} key={idx}/>
+            })}
         </div>
     )
 }
